958bcb6b15d58361027d3378ebd301f5f2da2322
Arrokoth <arrokoth233@gmail.com>
2023-10-27 12:06:44 +0200

    Initializing Mod

---
+++ src/main/java/team/dovecotmc/tacticalgraphicscard/tacticalgraphicscard/TacticalGraphicsCard.java
@@ -0,0 +1,15 @@
+package team.dovecotmc.tacticalgraphicscard.tacticalgraphicscard;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * @author Arrokoth
+ * @project Tactical-Graphics-Card
+ * @copyright Copyright © 2023 Arrokoth All Rights Reserved.
+ */
+public class TacticalGraphicsCard implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/team/dovecotmc/tacticalgraphicscard/tacticalgraphicscard/client/TacticalGraphicsCardClient.java
@@ -0,0 +1,15 @@
+package team.dovecotmc.tacticalgraphicscard.tacticalgraphicscard.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * @author Arrokoth
+ * @project Tactical-Graphics-Card
+ * @copyright Copyright © 2023 Arrokoth All Rights Reserved.
+ */
+public class TacticalGraphicsCardClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
